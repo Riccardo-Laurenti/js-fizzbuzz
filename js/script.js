1db375055fca57d2ad9573ce6b82a3681501ba0d
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-05-30 16:41:02 +0200

    push exercise for today --> no bonus

--- js/script.js
+++ js/script.js
@@ -4,4 +4,33 @@
 //  - Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 
 
-console.log('JSOK');
+
+
+
+// Recupero gli elementi in pagina
+
+const cicleElement = document.getElementById('cicle');
+
+
+let result ='';
+
+// Creo un ciclo che stampi in console i numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    
+    // Cambio risultato per i multipli di 3 o di 5
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        result += '<li>FizzBuzz</li>';
+    } else if (i % 3 === 0) {
+        result += '<li>Fizz</li>';
+    } else if (i % 5 === 0) {
+        result += '<li>Buzz</li>';
+    } else {
+        result += `<li>${i}</li>`;
+    }
+}
+
+console.log(`risultato fine ciclo`, result);
+
+// Stampo in pagina
+
+cicleElement.innerHTML = result;
--- steps.md
+++ steps.md
@@ -1,1 +1,7 @@
-## 1 CHIEDO ALL'UTENTE
+## 1 Recupero l'elemento list dal dom
+
+## 2 Scrivo un programma che stampi in console i numeri da 1 a 100
+
+## 3 Cambio risultato per i multipli di 3 o di 5
+
+## 4 Stampo in pagina
